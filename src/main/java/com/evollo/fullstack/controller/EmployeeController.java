991e3af59f8cd1ac6a8f2b8a98dfad467d3d8c40
Lucas Wilker <lwilker24@gmail.com>
2021-02-27 00:49:00 -0300

    Exceptions

--- src/main/java/com/evollo/fullstack/controller/EmployeeController.java
+++ src/main/java/com/evollo/fullstack/controller/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.evollo.fullstack.controller;
 
+import com.evollo.fullstack.exception.EmployeeNotFoundException;
 import com.evollo.fullstack.model.EmployeeModel;
 import com.evollo.fullstack.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,39 +18,34 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @GetMapping("/")
-    public List<EmployeeModel> getAll() throws Exception {
+    public List<EmployeeModel> getAll() {
         return employeeService.getAll();
     }
 
     @GetMapping("/employee/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public EmployeeModel get(@PathVariable("id") Long id) {
-        return employeeService.getByid(id);
+    public ResponseEntity<EmployeeModel> get(@PathVariable("id") Long id) throws EmployeeNotFoundException {
+        return employeeService.getById(id);
     };
 
     @PostMapping("/employee")
-    @ResponseStatus(HttpStatus.CREATED)
-    public EmployeeModel post(@RequestBody EmployeeModel employeeModel) {
+    public ResponseEntity<EmployeeModel> post(@RequestBody EmployeeModel employeeModel) {
         return employeeService.save(employeeModel);
     };
 
     @PutMapping("/employee/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public EmployeeModel put(@PathVariable("id") Long id, @RequestBody EmployeeModel newEmployeeModel) {
-        EmployeeModel oldEmployeeModel = employeeService.getByid(id);
-        return employeeService.update(oldEmployeeModel, newEmployeeModel);
+    public ResponseEntity<EmployeeModel> put(@PathVariable("id") Long id,
+                                             @RequestBody EmployeeModel newEmployeeModel) throws EmployeeNotFoundException {
+        return employeeService.update(id, newEmployeeModel);
     };
 
     @PatchMapping("/employee/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public EmployeeModel patch(@PathVariable("id") Long id, @RequestBody EmployeeModel newEmployeeModel) {
-        EmployeeModel oldEmployeeModel = employeeService.getByid(id);
-        return employeeService.update(oldEmployeeModel, newEmployeeModel);
+    public ResponseEntity<EmployeeModel> patch(@PathVariable("id") Long id,
+                                               @RequestBody EmployeeModel newEmployeeModel) throws EmployeeNotFoundException {
+        return employeeService.update(id, newEmployeeModel);
     }
 
     @DeleteMapping("/employee/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public String delete(@PathVariable("id") Long id) {
+    public ResponseEntity<EmployeeModel> delete(@PathVariable("id") Long id) throws EmployeeNotFoundException {
         return employeeService.delete(id);
     }
 
--- src/main/java/com/evollo/fullstack/service/EmployeeService.java
+++ src/main/java/com/evollo/fullstack/service/EmployeeService.java
@@ -1,8 +1,11 @@
 package com.evollo.fullstack.service;
 
+import com.evollo.fullstack.exception.EmployeeNotFoundException;
 import com.evollo.fullstack.model.EmployeeModel;
 import com.evollo.fullstack.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,15 +20,21 @@ public class EmployeeService {
         return employeeRepository.findAll();
     }
 
-    public EmployeeModel getByid(Long id) {
-        return employeeRepository.findByid(id);
+    public ResponseEntity<EmployeeModel> getById(Long id) throws EmployeeNotFoundException {
+        verifyIfEmployeeExists(id);
+        EmployeeModel employee = employeeRepository.findByid(id);
+        return new ResponseEntity<EmployeeModel>(employee, HttpStatus.OK);
     };
 
-    public EmployeeModel save(EmployeeModel employeeModel) {
-        return employeeRepository.save(employeeModel);
+    public ResponseEntity<EmployeeModel> save(EmployeeModel employeeModel) {
+        return new ResponseEntity<EmployeeModel>(employeeRepository.save(employeeModel), HttpStatus.CREATED) ;
     };
 
-    public EmployeeModel update(EmployeeModel oldEmployeeModel, EmployeeModel newEmployeeModel) {
+    public ResponseEntity<EmployeeModel> update(Long id, EmployeeModel newEmployeeModel) throws EmployeeNotFoundException {
+
+        verifyIfEmployeeExists(id);
+        EmployeeModel oldEmployeeModel = employeeRepository.findByid(id);
+
         if (newEmployeeModel.getName() != null) {
             oldEmployeeModel.setName(newEmployeeModel.getName());
         }
@@ -47,13 +56,21 @@ public class EmployeeService {
         }
 
         //update the old employee
-        return employeeRepository.save(oldEmployeeModel);
+        return new ResponseEntity<EmployeeModel>(employeeRepository.save(oldEmployeeModel), HttpStatus.OK);
     };
 
-    public String delete(Long id) {
-        EmployeeModel employeeModel = employeeRepository.findByid(id);
-        employeeRepository.delete(employeeModel);
-        return "Deleted Employee";
+    public ResponseEntity<EmployeeModel> delete(Long id) throws EmployeeNotFoundException {
+        verifyIfEmployeeExists(id);
+        EmployeeModel employee = employeeRepository.findByid(id);
+        employeeRepository.delete(employee);
+        return new ResponseEntity<EmployeeModel>(HttpStatus.NO_CONTENT);
+    }
+
+    private void verifyIfEmployeeExists(Long id) throws EmployeeNotFoundException {
+        EmployeeModel employee = employeeRepository.findByid(id);
+        if (employee == null) {
+            throw new EmployeeNotFoundException("Employee ID Not Found!");
+        }
     }
 
 }
