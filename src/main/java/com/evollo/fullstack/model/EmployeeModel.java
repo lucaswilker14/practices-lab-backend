a5eedce8ce49b97cdeb6e896f3a27751f3940249
Lucas Wilker <lwilker24@gmail.com>
2021-02-26 22:44:39 -0300

    Created new jpa model

--- src/main/java/com/evollo/fullstack/model/EmployeeModel.java
+++ src/main/java/com/evollo/fullstack/model/EmployeeModel.java
@@ -1,4 +1,30 @@
 package com.evollo.fullstack.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
 public class EmployeeModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String jobRole;
+
+    @Column(nullable = false)
+    private Float salary;
+
 }
